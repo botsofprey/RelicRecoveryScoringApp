d96e96bbb4fee08afe1609b936242fd7720a6914
doesItWorkYet <doesItWorkYet2016@gmail.com>
2017-09-22 15:00:40 -0400

    added scorekeeper, TODO: display score

--- app/src/main/java/com/example/jeremy/relicrecoveryscoringapp/MainActivity.java
+++ app/src/main/java/com/example/jeremy/relicrecoveryscoringapp/MainActivity.java
@@ -13,22 +13,23 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private long score; //yes, long cause we will dominate....
+    private int score; //yes, long, cuz we will dominate...
 
     public EditText autonomousGlyphStacked;
-    public CheckBox redJewel, blueJewel, autonomousGlyphColoumnBonus, autonomousInZone;
+    public CheckBox redJewel, blueJewel, autonomousGlyphColumnBonus, autonomousInZone;
     public ToggleButton team;
-
+    public TextView scoreTally;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //mTextMessage = (TextView) findViewById(R.id.message);
+        scoreTally = (TextView) findViewById(R.id.scoreTally);
         autonomousGlyphStacked = (EditText) findViewById(R.id.autonomousGlyphsStacked);
         redJewel = (CheckBox) findViewById(R.id.redJewel);
         blueJewel = (CheckBox) findViewById(R.id.blueJewel);
-        autonomousGlyphColoumnBonus = (CheckBox) findViewById(R.id.glyphColumnBonus);
+        autonomousGlyphColumnBonus = (CheckBox) findViewById(R.id.glyphColumnBonus);
         autonomousInZone = (CheckBox) findViewById(R.id.autonomousInZone);
         team = (ToggleButton) findViewById(R.id.team);
 
@@ -36,15 +37,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        //do scoring hear, just tally up points every time this is called...
+        //do scoring here, just tally up points every time this is called...
         score = 0;
         if(team.getText() == "Team Blue"){
+            if(autonomousGlyphColumnBonus.isChecked()){
+                score = score + 30;
+            }
+            if(autonomousInZone.isChecked()){
+                score = score + 10;
+            }
+            for(int x=0; x<1; x++){
+                String newString = autonomousGlyphStacked.getText().toString();
+                Integer result = Integer.valueOf(newString);
+                score = score + result;
+            }
 
         }
         else if(team.getText() == "Team Red"){
-
+            if(autonomousGlyphColumnBonus.isChecked()){
+                score = score + 30;
+            }
+            if(autonomousInZone.isChecked()){
+                score = score + 10;
+            }
+            if(redJewel.isChecked()){
+                score = score + 30;
+            }
+            for(int x=0; x<1; x++){
+                String newString = autonomousGlyphStacked.getText().toString();
+                Integer result = Integer.valueOf(newString);
+                score = score + result;
+            }
         }
-
-        //etc, etc
+        scoreTally.setText(String.format("Score: ", Integer.toString(score)));
     }
 }
