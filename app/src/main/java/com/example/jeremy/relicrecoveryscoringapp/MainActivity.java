0758cffa46f7bfb771d99d6871c74ef3fcb96f13
robotics <thebotsofprey@gmail.com>
2017-09-25 15:13:27 -0400

    stuff

--- app/src/main/java/com/example/jeremy/relicrecoveryscoringapp/MainActivity.java
+++ app/src/main/java/com/example/jeremy/relicrecoveryscoringapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jeremy.relicrecoveryscoringapp;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.IntegerRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -13,9 +14,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private int score; //yes, long, cuz we will dominate...
+    private int score = 0; //yes, long, cuz we will dominate...
+    Listener listener = new Listener();
 
     public EditText autonomousGlyphStacked;
     public CheckBox redJewel, blueJewel, autonomousGlyphColumnBonus, autonomousInZone;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Begin create", "");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //mTextMessage = (TextView) findViewById(R.id.message);
@@ -34,47 +37,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         autonomousGlyphColumnBonus = (CheckBox) findViewById(R.id.glyphColumnBonus);
         autonomousInZone = (CheckBox) findViewById(R.id.autonomousInZone);
         team = (ToggleButton) findViewById(R.id.team);
+        team.setOnClickListener(listener);
+        redJewel.setOnClickListener(listener);
+        blueJewel.setOnClickListener(listener);
+        autonomousGlyphColumnBonus.setOnClickListener(listener);
+        autonomousGlyphStacked.setOnClickListener(listener);
+        autonomousInZone.setOnClickListener(listener);
         Log.d("Created", "");
     }
 
-    @Override
-    public void onClick(View view) {
-        //do scoring here, just tally up points every time this is called...
-        score = 0;
-        if(team.getText() == "Team Blue"){
-            if(autonomousGlyphColumnBonus.isChecked()){
-                score = score + 30;
-            }
-            if(autonomousInZone.isChecked()){
-                score = score + 10;
-            }
-            if(blueJewel.isChecked() && !redJewel.isChecked()){
-                score = score + 30;
-            }
-            for(int x=0; x<1; x++){
-                String newString = autonomousGlyphStacked.getText().toString();
-                Integer result = Integer.valueOf(newString);
-                score = score + result;
-            }
+    public class Listener implements View.OnClickListener {
 
+        Listener(){}
+
+        @Override
+        public void onClick(View view){
+            score = 0;
+            switch (view.getId()){
+                default:
+                    if(team.getText().toString().equals("Team Red") && redJewel.isChecked() && !blueJewel.isChecked()) score += 30;
+                    else if(team.getText().toString().equals("Team Blue") && blueJewel.isChecked() && !redJewel.isChecked()) score += 30;
+                    if(autonomousGlyphColumnBonus.isChecked()) score += 30;
+                    if(autonomousInZone.isChecked()) score += 10;
+                    try {
+                        if(Integer.parseInt(autonomousGlyphStacked.getText().toString()) > 0){
+                            score += Integer.parseInt(autonomousGlyphStacked.getText().toString())*15;
+                        }
+                    } catch (Exception e){
+                        Log.e("Error", e.toString());
+                    }
+                    break;
+            }
+            Log.d("update score", "");
+            scoreTally.setText(Integer.toString(score));
         }
-        else if(team.getText() == "Team Red"){
-            if(autonomousGlyphColumnBonus.isChecked()){
-                score = score + 30;
-            }
-            if(autonomousInZone.isChecked()){
-                score = score + 10;
-            }
-            if(redJewel.isChecked() && !blueJewel.isChecked()){
-                score = score + 30;
-            }
-            for(int x=0; x<1; x++){
-                String newString = autonomousGlyphStacked.getText().toString();
-                Integer result = Integer.valueOf(newString);
-                score = score + result;
-            }
-        }
-        scoreTally.setText(Integer.toString(score));
-        Log.d("score:",Integer.toString(score));
     }
 }
