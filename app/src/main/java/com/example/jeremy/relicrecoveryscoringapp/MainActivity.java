ee464f503eacbe3e8cffa89b9eb5ecddd4d314cd
doesItWorkYet <doesItWorkYet2016@gmail.com>
2017-09-29 11:22:50 -0400

    Full app, mostly working

--- app/src/main/java/com/example/jeremy/relicrecoveryscoringapp/MainActivity.java
+++ app/src/main/java/com/example/jeremy/relicrecoveryscoringapp/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.jeremy.relicrecoveryscoringapp;
-import android.content.Intent;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
